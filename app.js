bc466d0de54d99515b355e0bbddce734643aff24
ASHISH KUMAR <ashishk8581@gmail.com>
2021-02-11 16:35:50 +0530

    display

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 const mysql = require('mysql');
-const ejs = require("ejs");
 var app = express();
+
 app.set('view engine', 'ejs');
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}))
+
 const PORT = process.env.PORT || 4000
 
 const db = mysql.createConnection({
@@ -23,27 +24,30 @@ db.connect((err) => {
     console.log('Mysql connected');
 })
 
-app.get('/', function(req,res) {
+app.get('/',function(req,res){
+    res.render('index',{text_old:"" ,text_new:""})
+})
+
+app.post('/convert', function(req,res) {
+    
     var text;
     var text_split;
-    text = req.body.Name;
+    text = req.body.word;
+    
     // text = "अफसर अफसरों दर्पण"
     text_split = text.split(" "); //splits the text into array
     var text_length  
     text_length = Object.keys(text_split).length; // length of array
 
-    db.query("SELECT * FROM urdu_to_hindi", function (err,result)
+    db.query("SELECT * FROM urdutohindi", function (err,result)
     {
         if(err)
         throw err
-
         var length_table = Object.keys(result).length
         var newstr = '';   //to store new string
-        var oldstr = '';  //to store old string
         for(var j=0; j < text_length; j++)
         {
             datas = text_split[j]
-            oldstr = oldstr + " " + datas  //pushing old text
             var check = 0
             for(var i=0 ; i < length_table; i++)
             {
@@ -59,19 +63,19 @@ app.get('/', function(req,res) {
                 newstr = newstr + " " + datas
             }       
         }
-        //  res.send({
-        //      'old':oldstr,
-        //      'new':newstr
-        //  })
-         res.render('index',{datas: {Converted : newstr, old: newstr2}}); //sending converted urdu to hindi sentence and urdu sentences
+         res.render('index',{text_new:newstr,text_old:""})
     })
+ 
+})
+
+app.post('/revert',function(req,res){
+    var text
+    text = req.body.word
+    console.log(text)
+    res.render('index',{text_old:text,text_new:""})
 })
  
 // setting server
 app.listen(PORT, () => {
     console.log(`listening to port ${PORT}`);
 })
-
-app.listen(3000,function(){
-    console.log("server running!");
-});
