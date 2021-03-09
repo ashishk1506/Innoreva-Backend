63e6ed4836d76b48ac0268f2fe0123dbd83b5171
Ashish Kumar <63970193+ashishk1506@users.noreply.github.com>
2021-03-09 12:28:54 +0530

    Update dbConfig.js

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host : 'benpmbp84xkrym4yz3bz-mysql.services.clever-cloud.com',
-    user : 'u1hql9occut1cyai',
-    password : 'kqeSj0Rc7xBCDdo7L5FL',
-    database: 'benpmbp84xkrym4yz3bz'
+    host : '',
+    user : '',
+    password : '',
+    database: ''
 })
 
 //connect
